89c9a50cb11255612df440079f335d16d1aea032
Zahra Vasanji <zahravasanji@hotmail.com>
2024-08-23 13:33:21 +0100

    journal styling

--- src/components/JournalEntry.jsx
+++ src/components/JournalEntry.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
+import "../styles/journalEntry.css";
 
 const JournalEntry = ({ journal }) => {
-    return (
-        <div >
-            <div>
-                <div>
-                    <p >{journal.date}</p>
-                    <h1 >{journal.title}</h1>
-                                <div >
-                <img src={journal.src} />
-            </div><p >{journal.description}</p>
-                </div>
-            </div>
+  return (
+    <div className="journalEntry">
+      <div className="imageContainer">
+        <img src={journal.src} />
+      </div>
+      <div className="textContainer">
+      <p className="date">{journal.date}</p>
+      <h1>{journal.title}</h1>
+      <p className="description">{journal.description}</p>
+    </div></div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default JournalEntry
+export default JournalEntry;
--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -5,7 +5,7 @@ import {useState} from 'react';
 const Journal = () => {
   const [journals, setJournals] = useState(journalList);
   return (
-    <div>
+    <div className="journals">
       {journals.map((journal) => (
         <JournalEntry journal={journal} key={journal.id}/>
       ))}
